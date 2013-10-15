14c954203e8a67abb16e1618ee42e2477f65a261
ptotem <info@ptotem.com>
2013-10-15 15:31:48 +0530

    oct 15 changes

--- config/initializers/schedule.rb
+++ config/initializers/schedule.rb
@@ -1,15 +1,13 @@
 require 'rubygems'
 require 'rufus/scheduler'  
 scheduler = Rufus::Scheduler.start_new
-scheduler.every("1s") do
-  puts 'Cron Started'
-    User.create(:email => "abc#{Time.now.to_i}@gmail.com", :username => params[:uname], :password => passwd, :password_confirmation => passwd)
-  puts 'Cron ended'   #User.score_update
-end
+# scheduler.every("1m") do
+#     User.score_update
+# end
 
-#scheduler.cron '38 1 * * *' do
-#    puts 'Cron Started'
-#    Response.decide_daily_winner
-#    User.send_response
-#    puts 'Cron ended'
-#end
+scheduler.cron '38 1 * * *' do
+    puts 'Cron Started'
+    Response.decide_daily_winner
+    User.send_response
+    puts 'Cron ended'
+end
