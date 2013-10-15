824f75037caa878749af89838f31c8f998557eb4
ptotem <info@ptotem.com>
2013-10-15 21:07:56 +0530

    changes done

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -274,8 +274,6 @@ class QuizController < ApplicationController
     @daily_winners.shuffle[0..9].each do |dw|
       DailyWinner.create(:question_id=>@question.id,:user_id=>dw,:is_display=>false)
     end
-    render :text=>@daily_winners
-    return
   end
 
   def daily_winner
