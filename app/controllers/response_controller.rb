244c9b21163adef08d05649ebced715350fccb8f
ptotem <info@ptotem.com>
2013-10-17 21:45:38 +0530

    all changes has been done

--- app/controllers/response_controller.rb
+++ app/controllers/response_controller.rb
@@ -728,7 +728,7 @@ class ResponseController < ApplicationController
       else
         @daily_winners=DailyWinner.find_all_by_question_id_and_is_display(@question.id,true)
         @daily_winners.first(5).each do |e|
-          @winners << "#{User.find(e.user_id).name}||#{User.find(e.user_id).username}"
+          @winners << "#{User.find(e.user_id).name}||#{User.find(e.user_id).username}||#{User.find(e.user_id).refer_points}"
         end
       end
       render :text => @winners
--- app/models/daily_winner.rb
+++ app/models/daily_winner.rb
@@ -1,5 +1,5 @@
 class DailyWinner < ActiveRecord::Base
-  attr_accessible :is_display, :question_id, :user_id
+  attr_accessible :is_display, :question_id, :user_id, :today_winners
 
   belongs_to :question
 end
