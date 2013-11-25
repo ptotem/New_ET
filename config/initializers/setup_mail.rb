fb876a599b10a3e22f71facd44a42870fd6d3adf
Rakesh Rengaraj <rakesh.rengaraj@gmail.com>
2013-11-25 11:14:44 +0530

    changes done for daily_winners and refer a friend

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,9 +1,19 @@
+#ActionMailer::Base.smtp_settings = {
+#    :address              => "smtp.timesgroup.in",
+#    :port                 => 25,
+#    :domain               => "timesgroup.in",
+#    :user_name            => "winwithet@timesgroup.in",
+#    :password             => "times@123",
+#    :authentication       => "plain",
+#    :enable_starttls_auto => true
+#}
+
 ActionMailer::Base.smtp_settings = {
-    :address              => "smtp.timesgroup.in",
-    :port                 => 25,
-    :domain               => "timesgroup.in",
-    :user_name            => "winwithet@timesgroup.in",
-    :password             => "times@123",
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => "ptotem.com",
+    :user_name            => "winwithet@gmail.com",
+    :password             => "p20o20e13",
     :authentication       => "plain",
     :enable_starttls_auto => true
 }
