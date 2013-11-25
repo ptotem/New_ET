8574fb454c1d13d72468f53ea9c51f942f7f416a
Rakesh Rengaraj <rakesh.rengaraj@gmail.com>
2013-11-25 13:25:28 +0530

    changes done as per mail

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,19 +1,9 @@
-#ActionMailer::Base.smtp_settings = {
-#    :address              => "smtp.timesgroup.in",
-#    :port                 => 25,
-#    :domain               => "timesgroup.in",
-#    :user_name            => "winwithet@timesgroup.in",
-#    :password             => "times@123",
-#    :authentication       => "plain",
-#    :enable_starttls_auto => true
-#}
-
 ActionMailer::Base.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
-    :domain               => "ptotem.com",
-    :user_name            => "winwithet@gmail.com",
-    :password             => "p20o20e13",
-    :authentication       => "plain",
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => "ptotem.com",
+    :user_name => "winwithet@gmail.com",
+    :password => "p20o20e13",
+    :authentication => "plain",
     :enable_starttls_auto => true
 }
