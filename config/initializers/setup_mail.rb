0a7bda29807ebb27fa3d57ad4649b392a02910b3
Rakesh Rengaraj <rakesh.rengaraj@gmail.com>
2013-11-26 11:21:38 +0530

    changes done

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -7,3 +7,17 @@ ActionMailer::Base.smtp_settings = {
     :authentication => "plain",
     :enable_starttls_auto => true
 }
+
+
+#ActionMailer::Base.raise_delivery_errors = true
+#ActionMailer::Base.delivery_method = :sendmail
+#ActionMailer::Base.smtp_settings = {
+#    :address        => 'mail.amv3.qlc.co.in',
+#    :port           => 25,
+#    :authentication => :plain,
+#    :user_name      => 'winwithet@timesgroup.in',
+#    :password       => 'times@123',
+#    :domain         => 'qlc.co.in',
+#    :enable_starttls_auto => true
+#}
+#ActionMailer::Base.default_url_options = { :host => 'http://amv3.qlc.co.in' }
